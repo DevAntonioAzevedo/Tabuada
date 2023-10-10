84fd5049480aa2eb2b7e5a6423d42271617c5314
Antonio Azevedo <antonio_azevedo@hotmail.com.br>
2023-10-09 21:36:07 -0300

    Criado botao DarkMode direto no app

--- app/src/main/java/com/example/tabuada/MainActivity.kt
+++ app/src/main/java/com/example/tabuada/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.tabuada
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.tabuada.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnDarkMode.setOnCheckedChangeListener { compoundButton, ischecked ->
+            if (ischecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
