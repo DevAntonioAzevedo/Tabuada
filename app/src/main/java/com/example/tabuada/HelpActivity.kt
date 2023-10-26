efda6b9007ad53e6a0694f41d9f7e8f31e0851d1
Antonio Azevedo <antonio_azevedo@hotmail.com.br>
2023-10-26 17:00:35 -0300

    Desenvolvendo a tela help

--- app/src/main/java/com/example/tabuada/HelpActivity.kt
+++ app/src/main/java/com/example/tabuada/HelpActivity.kt
@@ -1,11 +1,16 @@
 package com.example.tabuada
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HelpActivity : AppCompatActivity() {
+
+    private lateinit var tv_help: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+
     }
 }
