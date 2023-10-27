a5e14d03c803aad0059a4620255a04ed70360a14
Antonio Azevedo <antonio_azevedo@hotmail.com.br>
2023-10-27 15:40:26 -0300

    Concluido tela help

--- app/src/main/java/com/example/tabuada/HelpActivity.kt
+++ app/src/main/java/com/example/tabuada/HelpActivity.kt
@@ -1,16 +1,31 @@
 package com.example.tabuada
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HelpActivity : AppCompatActivity() {
 
     private lateinit var tv_help: TextView
+    private lateinit var btnClose:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+        setupView()
+        setupListeners()
 
+    }
+
+    fun setupView() {
+        btnClose = findViewById(R.id.iv_close)
+    }
+
+    // Funcao para fechar a tela help
+    fun setupListeners() {
+        btnClose.setOnClickListener {
+            finish()
+        }
     }
 }
